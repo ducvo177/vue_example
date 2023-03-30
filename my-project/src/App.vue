8de75097c00f83d650ff91b0251adf6e11ecc5ba
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-03-30 11:03:50 +0700

    vue_example

---
+++ my-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="unmountComponent">Unmount Component</button>
+    <my-component v-if="mounted"></my-component>
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue';
+
+export default {
+  components: {
+    MyComponent,
+  },
+  data() {
+    return {
+      mounted: true,
+    };
+  },
+  methods: {
+    unmountComponent() {
+      this.mounted = false;
+    },
+  },
+
+};
+</script>
---
+++ my-project/src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello from MyComponent!",
+    };
+  },
+  mounted() {
+    console.log("This component is mounted");
+  },
+  beforeUnmount() {
+   console.log("Before Unmount");
+   //Dùng để thực hiện các tác vụ như hủy các kết nối, xóa các listeners, hoặc lưu trữ trạng thái của component.
+  },
+  unmounted() {
+   console.log(" Unmounted");
+   //Dùng để thực hiện các tác vụ như giải phóng bộ nhớ, hoặc gửi log để theo dõi hoạt động của component.
+  },
+};
+</script>
